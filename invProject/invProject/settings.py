ca618fdf765d3cd90a1c2124635cfa3558bd800c
paramjeet2003 <157730644+paramjeet2003@users.noreply.github.com>
2024-06-28 00:14:32 +0530

    Update settings.py

--- invProject/invProject/settings.py
+++ invProject/invProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-79iq7*6aeh*m)o105f+3=a=h((pz^750$4b(-sm6rs1zg2fkmo'
+SECRET_KEY = '<SECRET_KEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
