76ed6e55439608b946fbb3e6d0b1b78a5dd49c80
paramjeet2003 <157730644+paramjeet2003@users.noreply.github.com>
2024-06-28 00:11:51 +0530

    Update settings.py

--- invProject/invProject/settings.py
+++ invProject/invProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-79iq7*6aeh*m)o105f+3=a=h((pz^750$4b(-sm6rs1zg2fkmo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
